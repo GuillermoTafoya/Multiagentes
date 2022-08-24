662c5ecec8962d49a7930ffde202dcee112001e1
Guillermo Tafoya <A01633790@tec.mx>
2022-08-23 23:23:42 -0500

    Working base

--- M3_Actividad (AD2022)/agents.py
+++ M3_Actividad (AD2022)/agents.py
@@ -6,8 +6,13 @@ class Car(Agent):
     def __init__(self, unique_id, model, colour=None, direction=None):
         super().__init__(unique_id, model)
         self.colour = colour
+        self.dx = 0
+        self.dy = 0
         self._direction = self.random.choice(self.DIRECTIONS) if not direction else direction
+        self.direction = self._direction
         self.alive = True
+        
+        self.next_pos = None
 
     @property
     def direction(self):
@@ -17,37 +22,18 @@ class Car(Agent):
     def direction(self, direction):
         self._direction = direction
         if self._direction == 'up':
-            self.dx, self.dy = -1, 0
-            return
-        if self._direction == 'down':
-            self.dx, self.dy = 1, 0
-            return
-        if self._direction == 'right':
             self.dx, self.dy = 0, 1
             return
-        if self._direction == 'left':
+        if self._direction == 'down':
             self.dx, self.dy = 0, -1
             return
+        if self._direction == 'right':
+            self.dx, self.dy = 1, 0
+            return
+        if self._direction == 'left':
+            self.dx, self.dy = -1, 0
+            return
         raise(ValueError('Invalid direction'))
-
-    def advance(self) -> None:
-        """
-        Defines a new state calculating the step of the model
-        """
-        neighbours = self.model.grid.get_neighbors(
-        self.pos,
-        moore = True,
-        include_center = True)
-        for neighbour in neighbours:
-            if isinstance(neighbour, Car) and self.pos == neighbour.pos:
-                # collision
-                neighbour.alive = False
-                self.alive = False
-                break
-            if isinstance(neighbour, TrafficLight) and neighbour.state == True and self.opositeDirections(neighbour.direction, self.direction):
-                # stop
-                return
-        self.model.grid.move_agent(self, self.next_state)
 
     def opositeDirections(self, direction1, direction2):
         if direction1 == 'up' and direction2 == 'down':
@@ -65,25 +51,44 @@ class Car(Agent):
         Defines how the model interacts within its environment.
         """
         # Check if the agent is alive
-        if not self.alive:
+        if not self.alive:   
             del self
             return
+
         # Look for traffic lights
-        neighbours = self.model.grid.get_neighbors(
-            self.pos,
-            moore=True,
-            include_center=True)
+        neighbours = self.model.grid.get_neighbors(self.pos, moore=False, include_center=True, radius=max(self.model.width, self.model.height))
+        
         for neighbour in neighbours:
+            
+            # Check collision with cars
+            if isinstance(neighbour, Car):
+                if self.pos == neighbour.pos:
+                    self.alive = False
+                    neighbour.alive = False
+                    del self
+                    del neighbour
+                    return
+            
             if isinstance(neighbour, TrafficLight):
                 if neighbour.state == True and self.opositeDirections(neighbour.direction, self.direction):
                     # stop
-                    return
+                    if (self.direction == 'up' or self.direction == 'down') and abs(neighbour.pos[1] - self.pos[1]) == 1:
+                        return
+                    if (self.direction == 'right' or self.direction == 'left') and abs(neighbour.pos[0] - self.pos[0]) == 1:
+                        return
+            # Try stopping if there is another car in the way
+            if isinstance(neighbour, Car):
+                if (self.direction == 'up' or self.direction == 'down') and abs(neighbour.pos[1] - self.pos[1]) == 1:
+                    if neighbour.pos[1] == self.pos[1] and neighbour.direction == self.direction:
+                        return
+                elif (self.direction == 'right' or self.direction == 'left') and abs(neighbour.pos[0] - self.pos[0]) == 1:
+                    if neighbour.pos[0] == self.pos[0] and neighbour.direction == self.direction:
+                        return
+
         # Move
         next_pos = (self.pos[0] + self.dx, self.pos[1] + self.dy)
-        if self.model.grid.out_of_bounds(next_pos):
-            self.next_pos = self.model.grid.torus_adj(next_pos)
-        else:
-            self.next_pos = next_pos
+        self.next_pos = next_pos
+        self.model.grid.move_agent(self, next_pos)
 
 class TrafficLight(Agent):
     """
@@ -98,7 +103,7 @@ class TrafficLight(Agent):
     def step(self):
         if self.model.schedule.steps % self.timeToChange == 0:
             self.state = not self.state
-        pass
+
 class Road(Agent):
     """
     Obstacle agent. Just to add obstacles to the grid.
--- M3_Actividad (AD2022)/models.py
+++ M3_Actividad (AD2022)/models.py
@@ -15,6 +15,47 @@ import random
 
 from agents import Car, TrafficLight, Road
 
+
+
+def get_grid(model):
+    grid = np.zeros((model.grid.width, model.grid.height))
+
+    #Por todas las celdas del grid
+    for cell in model.grid.coord_iter():
+        cell_content, x, y = cell        
+        for agent in cell_content:
+            #print(agent, x, y)
+            if isinstance(agent, Car):
+                if agent.colour == 'white':
+                    grid[x][y] = 6
+                elif agent.colour == 'blue':
+                    grid[x][y] = 7
+                elif agent.colour == 'purple':
+                    grid[x][y] = 8
+                else: # black
+                    grid[x][y] = 9
+            
+            elif isinstance(agent, TrafficLight):
+                if agent.state == True: # Green
+                    grid[x][y] = 2
+                else: # red
+                    grid[x][y] = 1
+
+            elif isinstance(agent, Road):
+                
+                if agent.colour == "brown":
+                    grid[x][y] = 3
+                elif agent.colour == 'olive':
+                    grid[x][y] = 4
+                else: # dark green
+                    grid[x][y] = 5
+            
+            else: # Street
+                grid[x][y] = 0
+
+    return grid
+
+
 class Board(Model):
     def __init__(self, width, height, N, seed=None):
         self.width = width
@@ -23,105 +64,58 @@ class Board(Model):
         self.schedule = SimultaneousActivation(self)
         self.running = True
         self.datacollector = DataCollector(
-            model_reporters={"Grid": get_grid}
-        )
+            model_reporters={"Grid": get_grid})
         random.seed(seed if seed is not None else time.time())
         self.N = N
         self.create_agents()
+    
+    def step(self):
         self.datacollector.collect(self)
+        self.schedule.step()
+
     def create_agents(self):
+        CarsId = 0
+        TrafficLightsId = self.N+1
         # Create roads
         # Makes a crossroad
-        for i in range(self.width):
-            for j in range(self.height):
-                if i == self.width // 2 or j == self.height // 2:
-                    road = Road((i, j), self, 'brown')
-                    self.grid.place_agent(road, (i, j))
+        for (content,x,y) in self.grid.coord_iter():
+            if x < self.width // 3 or x > self.width * 2 // 3:
+                if y < self.height // 3 or y > self.height * 2 // 3:
+                #print("1. Added road at", x, y)
+                    road = Road((x,y), self, colour = "olive")
+                    self.grid.place_agent(road, (x, y))
                     self.schedule.add(road)
-                """
-                else:
-                    road = Road((i, j), self, 'olive')
-                    self.grid.place_agent(road, (i, j))
-                    self.schedule.add(road)
-                """
-        # Create cars on the sides of the crossroad 
-        id = self.N
-        for i in range(self.width):
-            for j in range(self.height):
-                if i == self.width // 2 and j == self.height // 2:
-                    continue
-                if id == self.N:
-                    break
+
+            # Create cars on the sides of the crossroad             
+            if CarsId < self.N:
                 # Cars going up
-                if i == self.width // 2 or j == self.height // 2:
-                    car = Car(id, self, 'white', 'up')
-                    self.grid.place_agent(car, (i, j))
+                if x == 0 and abs(y - self.height // 2) < 2:
+                    #print("3. Added car at", x, y)
+                    car = Car(CarsId, self, direction = "right", colour = "white")
+                    self.grid.place_agent(car, (x, y))
                     self.schedule.add(car)
-                # Cars going from left to right
-                elif i < self.width // 2 and j < self.height // 2:
-                    car = Car(id, self, 'blue', 'right')
-                    self.grid.place_agent(car, (i, j))
+                    CarsId += 1
+                # Cars going down
+                elif abs(x - self.width // 2) < 2 and y == 0:
+                    #print("4. Added car at", x, y)
+                    car = Car(CarsId, self, direction = "up", colour = "blue")
+                    self.grid.place_agent(car, (x, y))
                     self.schedule.add(car)
-                id += 1
+                    CarsId += 1
         # Create traffic lights
         # Only two traffic lights, one from up to down and one from right to left
-        id = 0
-        for i in range(self.width):
-            for j in range(self.height):
-                if id == 3:
-                    break
-                if i == self.width // 2 and j == self.height // 2:
-                    traffic_light = TrafficLight(id, self, True, 10, "up")
-                    self.grid.place_agent(traffic_light, (i, j))
-                    self.schedule.add(traffic_light)
-                elif i == self.width // 2 and j == self.height // 2 - 1:
-                    traffic_light = TrafficLight(id, self, True, 10, "up")
-                    self.grid.place_agent(traffic_light, (i, j))
-                    self.schedule.add(traffic_light)
-                id += 1
-
-def get_grid(model):
-    grid = np.zeros((model.grid.width, model.grid.height))
-
-    #Por todas las celdas del grid
-    for cell in model.grid.coord_iter():
-        agent, x, y = cell
-
-        if isinstance(agent, Car):
-            if agent.colour == 'white':
-                grid[x][y] = 6
-            elif agent.colour == 'blue':
-                grid[x][y] = 7
-            elif agent.colour == 'purple':
-                grid[x][y] = 8
-            else: # black
-                grid[x][y] = 9
-
-        elif isinstance(agent, Road):
-            
-            if agent.colour == "brown":
-                grid[x][y] = 3
-            elif agent.colour == 'olive':
-                grid[x][y] = 4
-            else: # dark green
-                grid[x][y] = 5
-
-        elif isinstance(agent, TrafficLight):
-            if agent.state == True: # Green
-                grid[x][y] = 2
-            else: # red
-                grid[x][y] = 1
-
-        else: # Street
-            grid[x][y] = 0
-
-    return grid
-
-
+        trafficLight = TrafficLight(self.N + 1, self, direction = "left")
+        self.grid.place_agent(trafficLight, (self.width // 3, self.height // 3 * 2))
+        self.schedule.add(trafficLight)
+        
+        trafficLight = TrafficLight(self.N + 2, self, direction = "down")
+        self.grid.place_agent(trafficLight, (self.width // 3 * 2, self.height // 3))
+        self.schedule.add(trafficLight)
 
 if __name__ == '__main__':
     # Test if the model works
-    board = Board(20, 20, 10)
+    board = Board(10, 10, 10)
     for i in range(10):
         board.step()
-        print(board.datacollector.get_agent_vars_dataframe())
+        all = board.datacollector.get_model_vars_dataframe()
+        print(all.iloc[i]['Grid'])
