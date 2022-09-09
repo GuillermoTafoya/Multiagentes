59394bd217295f57f3842b67be0da86ad3dca91b
Guillermo Tafoya <A01633790@tec.mx>
2022-09-08 20:07:33 -0500

    IA_Model_Ready_For_Training

--- DQ Learning/IA_Trafficlight.py
+++ DQ Learning/IA_Trafficlight.py
@@ -1,4 +1,3 @@
-from turtle import forward
 import numpy as np
 import torch as torch
 import torch.nn as nn
@@ -8,39 +7,35 @@ import os
 from collections import deque
 import random
 import math
-from models import Board
-from agents import Agent
-
 # To be implemented
-class TrafficLightIA(Agent):
+class TrafficLightIA():
     """
-    #Agent to be trained using DQ learning.
+    # Agent IA to be trained using DQ learning.
     """
     class Q_Net(nn.Module):
         def __init__(self, input_size, output_size):
             super().__init__()
             # Process a 31x31x11 grid (11 channels)
-            self.fc1 = nn.Linear(input_size, 128)
-            self.fc2 = nn.Linear(128, 128)
-            # Return a 1x4 array with the Q values for the 4 trafficLights(the action is switching, for the 4 traffic lights)
-            # After the convolution, the output should be a 1x4
-            self.fc3 = nn.Linear(128, output_size)
+            # It is resized to 1x31x31x11
+            self.fc1 = nn.Linear(input_size, 256)
+            self.fc2 = nn.Linear(256, 256)
+            # Return an array with the Q values for the 4 trafficLights(each action represents a different permutation of the traffic lights)
+            # After the processing, the output should be a 1x16 array
+            self.fc3 = nn.Linear(256, output_size)
 
         def forward(self, x):
             x = F.relu(self.fc1(x))
             x = F.relu(self.fc2(x))
             x = self.fc3(x)
             return x
-
+        
         def save(self, path):
             torch.save(self.state_dict(), path)
 
-    def __init__(self, unique_id, model, n_episodes=1000, n_wins_objective=195, max_env_steps=None, gamma=1.0, epsilon=1.0,
+    def __init__(self, model, n_episodes=1000, n_wins_objective=100, max_env_steps=None, gamma=1.0, epsilon=1.0,
                     epsilon_min=0.01, epsilon_log_decay=0.995, alpha=0.01, alpha_decay=0.01, 
                     batch_size=64, quiet=False, saveweights=True):
-        super().__init__(unique_id, model)
         self.memory = deque(maxlen=100000)
-        
         self.env = model
         self.gamma = gamma
         self.epsilon = epsilon
@@ -52,64 +47,58 @@ class TrafficLightIA(Agent):
         self.n_wins_objective = n_wins_objective
         self.batch_size = batch_size
         self.quiet = quiet
+        self.criterion = torch.nn.MSELoss()
         self.saveweights=saveweights
         if max_env_steps is not None: self.env._max_episode_steps = max_env_steps
         
-        self.state = False
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.DQN = self.Q_Net(11*31*31, 4).to(self.device)
+
+        self.state = False
+        # Load DQN.pth if it exists
+        if not os.path.isfile("DQN.pth"):
+            self.DQN = self.Q_Net(11*31*31, 16).to(self.device)
+        else:
+            self.DQN = self.Q_Net(11*31*31, 16).to(self.device)
+            self.DQN.load_state_dict(torch.load("DQN.pth"))
+
+        self.opt = torch.optim.Adam(self.DQN.parameters(), lr=0.01)
     
     def get_epsilon(self, t):
         return max(self.epsilon_min, min(self.epsilon, 1.0 - math.log10((t + 1) * self.epsilon_decay)))
 
     def getMoves(self, state, epsilon = 1000):
         """
-        Return the Q values for each action (on or off for the 4 traffic lights)
+        Return the Q values for each action
         """
         if (np.random.random() <= epsilon):
-            # Select a random action
-            return np.random.randint(0, 2, size=4)
+            return np.random.randint(0, 16),True
         else:
             with torch.no_grad():
-                prediction = self.DQN(state)
-                # We return all the Q values, and each agent will choose if it switches or not
-                # depending on its corresponding Q value
-                # Return an array of 0s and 1s, 1 if the agent switches, 0 if it doesn't
-                return prediction.cpu().numpy()
+                return torch.argmax(self.DQN(state)).cpu().numpy(),False
 
     def remember(self, state, action, reward, next_state, done, e): ###
         reward = torch.tensor(reward).to(self.device)
         self.memory.append((state, action, reward, next_state, done, e))
     
     def replay(self, batch_size):
+        """
+        Replay the memory to train the DQN
+        """
         y_batch, y_target_batch = [], []
         minibatch = random.sample(self.memory, min(len(self.memory), batch_size))
-
-        for state, action, reward, next_state, done, e in minibatch: ###
+        for state, action, reward, next_state, done, e in minibatch:
             y = self.DQN(state)
             y_target = y.clone().detach()
             with torch.no_grad():
-                switches = self.getMove(next_state, self.get_epsilon(e))
-                # We get the Q values for the next state
-                y_target_next = self.DQN(next_state)
-                # We get the Q value for the action that was taken
-                Q_next = y_target_next[switches]
-                # We get the Q value for the action that was taken
-                Q_next = Q_next.max(1)[0].view(1, 1)
-                # If the episode is done, we don't add the Q value of the next state
-                if done:
-                    y_target[0][action] = reward
-                else:
-                    y_target[0][action] = reward + self.gamma * Q_next
-            y_batch.append(y)
-            y_target_batch.append(y_target)
+                y_target[0][action] = reward if done else reward + self.gamma * torch.max(self.DQN(next_state)[0])
+            y_batch.append(y[0])
+            y_target_batch.append(y_target[0])
         y_batch = torch.cat(y_batch)
         y_target_batch = torch.cat(y_target_batch)
-        loss = F.mse_loss(y_batch, y_target_batch)
-        self.DQN.zero_grad()
+        self.opt.zero_grad()
         loss = self.criterion(y_batch, y_target_batch)
         loss.backward()
-        self.opt.step()        
+        self.opt.step()
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
     def train(self):
@@ -120,17 +109,30 @@ class TrafficLightIA(Agent):
         for e in range(self.n_episodes):
             state = self.env.reset()
             state = torch.tensor(state).to(self.device)
-            state = torch.reshape(state, (1, 11, 31, 31))
+            # We reshape the state to be a 1x(11x31x31) tensor
+            state = state.view(1, 11*31*31)
+            print("-"*50)
+            print("Episode: ", e)
+            print("-"*50)
+            state = state.float()
             score = 0
             done = False
+            steps = 0
             while not done:
-                switches = self.getMoves(state, self.get_epsilon(e))
-                next_state, reward, done, _ = self.env.step(switches)
+                action,wasRandom = self.getMoves(state, self.get_epsilon(e))
+                # Give time so the cars can move
+                for _ in range(4): 
+                    next_state, reward, done, _ = self.env.step(action)
                 next_state = torch.tensor(next_state).to(self.device)
-                next_state = torch.reshape(next_state, (1, 11, 31, 31))
-                self.remember(state, switches, reward, next_state, done, e)
+                next_state = next_state.view(1,11*31*31)
+                next_state = next_state.float()
+                if steps % 50 == 0:
+                    print(f'[{e}] [{steps}]',"Action: ", action, "| Reward: ", reward, "| Was random action:", wasRandom)
+                    print("Successful Trips:", self.env.successful_trips, "| Crashes:", self.env.crashes)
+                self.remember(state, action, reward, next_state, done, e)
                 state = next_state
                 score += reward
+                steps += 1
             scores.append(score)
             mean_score = np.mean(scores)
             if mean_score >= self.n_wins_objective and e >= 100:
@@ -138,7 +140,7 @@ class TrafficLightIA(Agent):
                 if self.saveweights: torch.save(self.DQN.state_dict(), 'DQN.pth')
                 break
             if e % 100 == 0 and not self.quiet:
-                print('[Episode {}] - Mean survival time over last 100 episodes was {} ticks.'.format(e, mean_score))
+                print('[Episode {}] - Mean score over last 100 episodes was {}.'.format(e, mean_score))
             self.replay(self.batch_size)
             self.Q_Net.save(self.DQN, 'DQN.pth')
         if not self.quiet: print('Did not solve after {} episodes 😞'.format(e))
--- DQ Learning/models_DQ.py
+++ DQ Learning/models_DQ.py
@@ -10,33 +10,42 @@ matplotlib.rcParams['animation.embed_limit'] = 2 ** 128
 import numpy as np
 import pandas as pd
 import time
+from itertools import product
 import random
-from agents import Car, TrafficLight, Road
-
+from agents_AI import Car, TrafficLight, Road
 def get_grid(model):
     grid = np.zeros((model.grid.width, model.grid.height))
     if model.write:
-        stepJson = "\n{"
-        carsJson = "\"cars\": [\n"
-        trafficLightsJson = "\"trafficLights\": [\n"
+        verticalJson = "["
+        horizontalJson = "["
     for cell in model.grid.coord_iter():
         cell_content, x, y = cell        
         for agent in cell_content:
             # Save relevant agents to json
             if isinstance(agent, Car):
-                if model.write:
-                    carsJson += "{\"id\":" + str(agent.unique_id) + ",\n\t\"x\":" + str(x) + ",\n\t\"y\":" + str(y) + ",\n\t\"direction\":\"" + agent.direction + "\"\n\t},\n"
+                
                 if agent.direction == 'up':
+                    if model.write:
+                        verticalJson += '{'+f'"{agent.unique_id}":{{"x":{x},"y":{y},"speed":{agent.dx,agent.dy},"direction":"{agent.direction}"}}'+'},'
                     grid[x][y] = 4
                 elif agent.direction == 'down':
+                    if model.write:
+                        verticalJson += '{'+f'"{agent.unique_id}":{{"x":{x},"y":{y},"speed":{agent.dx,agent.dy},"direction":"{agent.direction}"}}'+'},'
                     grid[x][y] = 5
                 elif agent.direction == 'left':
+                    if model.write:
+                        horizontalJson += '{'+f'"{agent.unique_id}":{{"x":{x},"y":{y},"speed":{agent.dx,agent.dy},"direction":"{agent.direction}"}}'+'},'
                     grid[x][y] = 6
                 elif agent.direction == 'right':
+                    if model.write:
+                        horizontalJson += '{'+f'"{agent.unique_id}":{{"x":{x},"y":{y},"speed":{agent.dx,agent.dy},"direction":"{agent.direction}"}}'+'},'
                     grid[x][y] = 7
             elif isinstance(agent, TrafficLight):
                 if model.write:
-                    trafficLightsJson += "{\"id\":" + str(agent.unique_id) + ",\n\t\"x\":" + str(x) + ",\n\t\"y\":" + str(y) + ",\n\t\"direction\":\"" + agent.direction + "\",\n\t\"state\":" + str(agent.state).lower() + "\n\t},\n"
+                    if agent.direction == 'up' or agent.direction == 'down':
+                        verticalJson += '{'+f'"{agent.unique_id}":{{"x":{x},"y":{y},"state":"{agent.state}","direction":"{agent.direction}"}}'+'},'
+                    else:
+                        horizontalJson += '{'+f'"{agent.unique_id}":{{"x":{x},"y":{y},"state":"{agent.state}","direction":"{agent.direction}"}}'+'},'
                 if agent.state == True:
                     grid[x][y] = 1
                 else:
@@ -55,12 +64,11 @@ def get_grid(model):
                 if x < model.grid.width//3 or x > 2*model.grid.width//3:
                     grid[x][y] = 8
     if model.write:
-        carsJson = carsJson[:-2] + "\n],\n"
-        trafficLightsJson = trafficLightsJson[:-2] + "\n]"
-        stepJson += carsJson + trafficLightsJson + "\n},"
+        verticalJson = verticalJson[:-1]+"]"
+        horizontalJson = horizontalJson[:-1]+"]"
         # Append json to file
-        with open("data.json", "a") as file:
-            file.write(stepJson)
+        with open("data.txt", "a") as file:
+            file.write(verticalJson + horizontalJson + "\n")
     return grid
 
 def get_grid_one_hot(model):
@@ -106,18 +114,18 @@ def get_grid_one_hot(model):
                     grid[x][y][10] = 1
     return grid
 class Board(Model):
-    def __init__(self, height, width, seed=None, spawn_rate = 1, max_spawn_batch = 1, one_hot = False, write = False, IA = False):
+    def __init__(self, height, width, seed=None, spawn_rate = 1, max_spawn_batch = 1, one_hot = False, write = False,targetSuccessfulTrips = 200):
         self.width = height
         self.height = width
         self.write = write
         self.grid = MultiGrid(height, width, torus=False)
         self.schedule = SimultaneousActivation(self)
         self.running = True
-        self.IA = IA
-        if not self.IA:
-            self.datacollector = DataCollector(
-                model_reporters={"Grid": get_grid_one_hot if one_hot else get_grid
-                })
+        # All possible ways in which traffic lights can be (on or off)
+        self.actionTranslator = list(product([True, False], repeat=4))
+        self.targetSuccessfulTrips = targetSuccessfulTrips
+        #self.datacollector = DataCollector(
+        #    model_reporters={"Grid": get_grid_one_hot if one_hot else get_grid})
         random.seed(seed if seed is not None else time.time())
         self.carID = 4
         self.spawn_rate = spawn_rate
@@ -137,19 +145,25 @@ class Board(Model):
             if isinstance(agent, Car):
                 self.schedule.remove(agent)
         self.create_agents()
+        return get_grid_one_hot(self)
     def rewardFunction(self):
         """
         Reward function for DQ learning
         """
-        return self.successful_trips*20 - self.crashes*100 - self.time_stuck*0.5
-    def step(self):
+        return self.successful_trips*30 - self.crashes*100 - self.time_stuck*2
+    def step(self,traffic_light_decisions:int):
         if self.schedule.steps % self.spawn_rate == 0:
             for _ in range(random.randint(1, self.max_spawn_batch)):
                 self.spawn_random_car()
-        if not self.IA:
-            self.datacollector.collect(self)
+        # Update traffic lights according to the Q values
+        # First, decide if we want to change the traffic light (the action is switching)
+        id = 0
+        actionsTaken = self.actionTranslator[traffic_light_decisions]
         self.schedule.step()
         for agent in self.schedule.agents:
+            if isinstance(agent, TrafficLight) and agent.unique_id == id:
+                agent.state = actionsTaken[id]
+                id += 1
             if isinstance(agent, Car):
                 if not agent.alive:
                     self.crashes += 0.5 # 0.5 because it counts both cars
@@ -166,6 +180,8 @@ class Board(Model):
                 if agent.stopped:
                     self.time_stuck += 1
                     continue
+        # next_state, reward, done, _ = self.env.step(switches)
+        return get_grid_one_hot(self), self.rewardFunction(), self.successful_trips > self.targetSuccessfulTrips, {}
         
     def spawn_random_car(self):
         direction = random.choice(['left', 'right', 'up', 'down'])
